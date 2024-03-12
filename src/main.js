6fc42518b18cdc01ea4e3a6b798424cdbfc7d404
whwjdan <whwjdan@naver.com>
2024-03-12 21:35:17 +0900

    feat: 로그인 화면 추가

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import NshopHome from "@/pages/NshopHome.vue";
+import NshopLogin from "@/pages/NshopLogin.vue";
 
 const routes = [
     {path: '/', component:NshopHome},
+    {path: '/login', component:NshopLogin},
 ]
 
 const router = createRouter({
