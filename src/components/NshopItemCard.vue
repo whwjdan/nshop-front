5e40c398bdbef1de91349a05d75e99df85ee36e7
whwjdan <whwjdan@naver.com>
2024-03-12 21:06:37 +0900

    feat: 상품 페이지 변경 및 백엔드 연결

--- src/components/NshopItemCard.vue
+++ src/components/NshopItemCard.vue
@@ -2,22 +2,39 @@
   <div class="card shadow-sm">
     <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
     <div class="card-body">
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text">
+        <span>{{ item.name }} &nbsp;</span>
+        <small class="discount badge bg-danger">
+          {{ item.discountPer}}%
+        </small>
+      </p>
       <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-body-secondary">9 mins</small>
+        <button class="btn btn-primary">구입하기</button>
+        <small class="price text-body-secondary">
+          {{ lib.getNumberFormatted(item.price) }}원
+        </small>
+        <small class="real text-danger">
+          {{ lib.getNumberFormatted(item.price - (item.price * item.discountPer / 100)) }}원
+        </small>
       </div>
     </div>
   </div>
 </template>
 <script>
+import lib from "@/scripts/lib";
+
 export default {
   name: 'NshopItemCard',
+  props:{
+    item: String
+  },
+  setup(){
+    return {lib}
+  }
 }
 </script>
 <style scoped>
-
+.card .card-body .price {
+  text-decoration: line-through;
+}
 </style>
--- src/pages/NshopHome.vue
+++ src/pages/NshopHome.vue
@@ -1,24 +1,10 @@
 <template>
   <div class="home">
-    <section class="py-5 text-center container">
-      <div class="row py-lg-5">
-        <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light">Album example</h1>
-          <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </div>
-    </section>
-
-    <div class="album py-5 bg-body-tertiary">
+    <div class="album py-5 bg-light">
       <div class="container">
-
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <NshopItemCard/>
+          <div class="col" v-for="(item, idx) in state.items" :key="idx">
+            <NshopItemCard :item="item"/>
           </div>
         </div>
       </div>
@@ -27,10 +13,27 @@
 </template>
 
 <script>
+import axios from "axios";
+import {reactive} from "vue";
 import NshopItemCard from "@/components/NshopItemCard.vue";
 
 export default {
-  name: 'NshopHome',
-  components: {NshopItemCard}
+  name: "NshopHome",
+  components: {NshopItemCard},
+  setup() {
+    const state = reactive({
+      items: []
+    })
+
+    axios.get("/api/items").then(({data}) => {
+      state.items = data;
+    });
+
+    return {state}
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
